e8ebef6e5285d4fd991f7289d94797ef582141b0
Glitch (thirsty-debonair-hugger) <none>
2022-01-25 01:35:24 +0000

    🏏👁 Checkpoint ./style.css:41359452/2806 ./script.js:41359452/1529 ./index.html:41359452/3765

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-const image_input = document.querySelector('#image_input');
+const URL = 'https://teachablemachine.withgoogle.com/models/8zi2D36nX/';
+const upload_an_image = document.querySelector('#image_input');
 const try_me = document.querySelector('#try_me');
-let uploaded_image;
+let imageFile, uploaded_image, model, result;
 
-//ml5.classification
+const modelURL = URL + 'model.json';
+const metadataURL = URL + 'metadata.json';
 
-image_input.addEventListener('change', function () {
+async function app() {
+  console.log('Loading model..');
+
+  // Load the model.
+  model = await tmImage.load(modelURL, metadataURL);
+  console.log('Successfully loaded model');
+
+  const imgEl = uploaded_image;
+  console.log('Is this image?', imgEl);
+
+  // Make a prediction through the model on our image.
+  console.log('Image being scanned.');
+  result = await model.predict(imgEl);
+
+  let bread = result[0];
+  let dog = result[1];
+
+  if (bread['probability'] >= dog['probability']) {
+    console.log('BREAD');
+    document.getElementById('print_answer').innerHTML = 'BREAD';
+  } else {
+    console.log('DOG');
+    document.getElementById('print_answer').innerHTML = 'DOG';
+  }
+}
+
+upload_an_image.addEventListener('change', function () {
   const reader = new FileReader();
   reader.addEventListener('load', () => {
     uploaded_image = reader.result;
-    document.querySelector(
-      '#display_image'
-    ).style.backgroundImage = `url(${uploaded_image})`;
+    document.querySelector('#display_image').src = uploaded_image;
+    document.getElementById('print_answer').innerHTML = '';
   });
 
+  imageFile = this.files[0];
   reader.readAsDataURL(this.files[0]);
 });
 
 try_me.addEventListener('click', function () {
-  if (uploaded_image) {
-    console.log('Try Me Clicked!');
-  } else {
-    console.log('Nothing to test!');
-  }
+
+  uploaded_image = document.getElementById('display_image');
+  console.log('Try Me Clicked!');
+  app();
 });
